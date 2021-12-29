ddf5532345093afb301b3d9699f846198c0cc451
Giulia Ghisini <giulia.ghisini@redturtle.it>
2021-12-29 10:58:13 +0100

    chore: added CookieGroupSettings component

---
+++ src/components/CookieBanner/CookieGroupSettings.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { defineMessages, useIntl } from 'react-intl';
+import { Radio } from 'semantic-ui-react';
+import { getLocaleConf, getCookiesKeys } from '../../helpers/config';
+import { groupIsAccepted } from '../../helpers/banner';
+import config from '@plone/volto/registry';
+
+const messages = defineMessages({
+  enabled: {
+    id: 'volto-gdpr-privacy-enabled',
+    defaultMessage: 'Enabled',
+  },
+  disabled: {
+    id: 'volto-gdpr-privacy-disabled',
+    defaultMessage: 'Disabled',
+  },
+});
+
+const CookieGroupSettings = ({
+  groupConfig,
+  disabled = false,
+  preferences,
+  setPreferences,
+}) => {
+  const intl = useIntl();
+
+  const text = getLocaleConf(groupConfig.text, config, intl.locale);
+  const isAccepted = groupIsAccepted(groupConfig, preferences);
+
+  const onChangeGroup = (groupConfig, value) => {
+    const keys = getCookiesKeys(groupConfig);
+    let newPreferences = { ...preferences };
+    keys.forEach((k) => {
+      newPreferences[k] = value;
+    });
+
+    setPreferences(newPreferences);
+  };
+
+  return (
+    <div className="settings-group-wrapper">
+      <div className="settings-title">
+        <div className="toggle-wrapper">
+          {text.title}
+          <Radio
+            toggle
+            checked={isAccepted}
+            disabled={disabled}
+            aria-label={
+              isAccepted
+                ? intl.formatMessage(messages.enabled)
+                : intl.formatMessage(messages.disabled)
+            }
+            onChange={(e, { checked }) => {
+              onChangeGroup(groupConfig, checked);
+            }}
+          />
+        </div>
+      </div>
+
+      {text.description && (
+        <div className="settings-description">{text.description}</div>
+      )}
+
+      <div className="choices">
+        {groupConfig.choices.map((choiceConfig) => {
+          const key = choiceConfig.config_key;
+          const choice = getLocaleConf(choiceConfig.text, config, intl.locale);
+
+          return (
+            <div className="choice" key={key}>
+              <div className="choice-title">
+                <div className="toggle-wrapper">
+                  {choice.title}
+                  <Radio
+                    toggle
+                    checked={preferences[key]}
+                    disabled={disabled}
+                    aria-label={
+                      preferences[key]
+                        ? intl.formatMessage(messages.enabled)
+                        : intl.formatMessage(messages.disabled)
+                    }
+                    onChange={(e, { checked }) => {
+                      setPreferences({
+                        ...preferences,
+                        [key]: checked,
+                      });
+                    }}
+                  />
+                </div>
+              </div>
+
+              {choice.description && (
+                <div className="choice-description">{choice.description}</div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default CookieGroupSettings;
--- src/components/CookieBanner/CookieSettings.jsx
+++ src/components/CookieBanner/CookieSettings.jsx
@@ -3,6 +3,7 @@ import { defineMessages, useIntl } from 'react-intl';
 import { Radio } from 'semantic-ui-react';
 import { getLocaleConf, getCookiesKeys } from '../../helpers/config';
 import { groupIsAccepted } from '../../helpers/banner';
+import CookieGroupSettings from './CookieGroupSettings';
 import config from '@plone/volto/registry';
 
 const messages = defineMessages({
@@ -48,143 +49,26 @@ const CookieSettings = ({ preferences, setPreferences, cookiesConfig }) => {
     preferences,
   );
 
-  console.log('preferrences', preferences);
   return (
     <div className="gdpr-privacy-settings">
       {/******** TECHNICAL ********/}
       <div className="settings-column technical">
-        <div className="settings-title">
-          <div className="toggle-wrapper">
-            {technicalText.title}{' '}
-            <Radio
-              toggle
-              checked={technicalIsAccepted}
-              disabled
-              aria-label={
-                technicalIsAccepted
-                  ? intl.formatMessage(messages.enabled)
-                  : intl.formatMessage(messages.disabled)
-              }
-              onChange={(e, { checked }) => {
-                onChangeGroup(cookiesConfig.technical, checked);
-              }}
-            />
-          </div>
-        </div>
-
-        {technicalText.description && (
-          <div className="settings-description">
-            {technicalText.description}
-          </div>
-        )}
-
-        <div className="choices">
-          {cookiesConfig.technical.choices.map((choiceConfig) => {
-            const key = choiceConfig.config_key;
-            const choice = getLocaleConf(
-              choiceConfig.text,
-              config,
-              intl.locale,
-            );
-
-            return (
-              <div className="choice" key={key}>
-                <div className="choice-title">
-                  <div className="toggle-wrapper">
-                    {choice.title}
-                    <Radio
-                      toggle
-                      checked={preferences[key]}
-                      disabled
-                      aria-label={
-                        preferences[key]
-                          ? intl.formatMessage(messages.enabled)
-                          : intl.formatMessage(messages.disabled)
-                      }
-                      onChange={(e, { checked }) => {
-                        setPreferences({
-                          ...preferences,
-                          [key]: checked,
-                        });
-                      }}
-                    />
-                  </div>
-                </div>
-
-                {choice.description && (
-                  <div className="choice-description">{choice.description}</div>
-                )}
-              </div>
-            );
-          })}
-        </div>
+        <CookieGroupSettings
+          groupConfig={cookiesConfig.technical}
+          disabled={true}
+          preferences={preferences}
+          setPreferences={setPreferences}
+        />
       </div>
 
       {/******** PROFILING ********/}
       <div className="settings-column profiling">
-        <div className="settings-title">
-          <div className="toggle-wrapper">
-            {profilingText.title}
-            <Radio
-              toggle
-              checked={profilingIsAccepted}
-              aria-label={
-                profilingIsAccepted
-                  ? intl.formatMessage(messages.enabled)
-                  : intl.formatMessage(messages.disabled)
-              }
-              onChange={(e, { checked }) => {
-                onChangeGroup(cookiesConfig.profiling, checked);
-              }}
-            />
-          </div>
-        </div>
-
-        {profilingText.description && (
-          <div className="settings-description">
-            {profilingText.description}
-          </div>
-        )}
-
-        <div className="choices">
-          {cookiesConfig.profiling.choices.map((choiceConfig) => {
-            const key = choiceConfig.config_key;
-            const choice = getLocaleConf(
-              choiceConfig.text,
-              config,
-              intl.locale,
-            );
-
-            return (
-              <div className="choice" key={key}>
-                <div className="choice-title">
-                  <div className="toggle-wrapper">
-                    {choice.title}
-                    <Radio
-                      toggle
-                      checked={preferences[key]}
-                      aria-label={
-                        preferences[key]
-                          ? intl.formatMessage(messages.enabled)
-                          : intl.formatMessage(messages.disabled)
-                      }
-                      onChange={(e, { checked }) => {
-                        setPreferences({
-                          ...preferences,
-                          [key]: checked,
-                        });
-                      }}
-                    />
-                  </div>
-                </div>
-
-                {choice.description && (
-                  <div className="choice-description">{choice.description}</div>
-                )}
-              </div>
-            );
-          })}
-        </div>
+        <CookieGroupSettings
+          groupConfig={cookiesConfig.profiling}
+          disabled={false}
+          preferences={preferences}
+          setPreferences={setPreferences}
+        />
       </div>
     </div>
   );
