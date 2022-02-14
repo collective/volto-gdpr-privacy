39d6bee7ebc1835512f54e38e3319b71d7ad802f
mamico <mauro.amico@gmail.com>
2022-02-14 22:56:13 +0100

    feat: disable cookiebanner with pagespeed/lighthouse

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,10 +3,18 @@ import Cookies from './Cookies';
 import { getLocaleConf, getCookiesKeys } from './config';
 import { groupIsAccepted } from './banner';
 
+const isPageSpeedBot = () => {
+  return (
+    navigator.userAgent.indexOf('Speed Insights') > -1 ||
+    navigator.userAgent.indexOf('Chrome-Lighthouse') > -1
+  );
+};
+
 export {
   usePanelConfigAndPreferences,
   Cookies,
   getLocaleConf,
   getCookiesKeys,
   groupIsAccepted,
+  isPageSpeedBot,
 };
