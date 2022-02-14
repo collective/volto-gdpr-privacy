b9fc1119a0f149f6fdfb7aa1c72fbd28e3a25a35
Giulia Ghisini <giulia.ghisini@redturtle.it>
2022-02-14 17:26:56 +0100

    fix: fix cookies

--- src/helpers/Cookies.js
+++ src/helpers/Cookies.js
@@ -1,33 +1,35 @@
-import Cookies as UniversalCookies from "universal-cookie";
-import config from "@plone/volto/registry";
+import { Cookies as UniversalCookies } from 'universal-cookie';
+import config from '@plone/volto/registry';
 
-export const COOKIES_PREFIX = "vgdpr_";
+export const COOKIES_PREFIX = 'vgdpr_';
 
 export const getExpirationDate = (date = null, expiringDays) => {
   const days =
     expiringDays ??
-    config.settings["volto-gdpr-privacy"]?.cookieExpires ??
+    config.settings['volto-gdpr-privacy']?.cookieExpires ??
     6 * 30; //default: 6 month
   const expireDate = date ? new Date(date) : new Date();
-  expireDate.setTime(expireDate.getTime() + expiringDays * 24 * 60 * 60 * 1000);
+  expireDate.setTime(expireDate.getTime() + days * 24 * 60 * 60 * 1000);
   return expireDate;
 };
 
 export default class Cookies {
-  cookies = new UniversalCookies();
+  constructor() {
+    this.cookies = new UniversalCookies();
+  }
 
   get(name) {
-    return cookies.get(COOKIES_PREFIX + name);
+    return this.cookies.get(COOKIES_PREFIX + name);
   }
 
   set(name, value, cookieExpiration) {
-    cookies.set(COOKIES_PREFIX + name, value, {
+    this.cookies.set(COOKIES_PREFIX + name, value, {
       expires: cookieExpiration || getExpirationDate(),
-      path: "/",
+      path: '/',
     });
   }
 
   remove(name) {
-    cookies.remove(COOKIES_PREFIX + name, { path: "/" });
+    this.cookies.remove(COOKIES_PREFIX + name, { path: '/' });
   }
 }
