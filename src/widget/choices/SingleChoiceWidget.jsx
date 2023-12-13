6b32ea7cd9a643ed9e3610a44e52be68128dc4ad
Giulia Ghisini <giulia.ghisini@redturtle.it>
2023-12-13 11:16:18 +0100

    fix:referenceurl widget

--- src/widget/choices/SingleChoiceWidget.jsx
+++ src/widget/choices/SingleChoiceWidget.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { defineMessages, useIntl } from 'react-intl';
 import { Grid, Segment, Header } from 'semantic-ui-react';
 import { MultilingualWidget } from 'volto-multilingual-widget';
-import { TextWidget, ArrayWidget } from '@plone/volto/components';
+import { TextWidget } from '@plone/volto/components';
+import TokenWidget from '@plone/volto/components/manage/Widgets/TokenWidget';
 import SlateRichTextWidget from '@plone/volto-slate/widgets/RichTextWidget';
 import ChoiceTextWidget from './ChoiceTextWidget';
 import ReferenceUrlsWidget from './ReferenceUrlsWidget';
@@ -19,6 +20,14 @@ const messages = defineMessages({
   referenceUrls: {
     id: 'gdprcookiesettings-choice_referenceUrls',
     defaultMessage: 'Reference URLs',
+  },
+  referenceUrls_placeholder: {
+    id: 'gdprcookiesettings-choice_referenceUrls_placeholder',
+    defaultMessage: 'Type URL domains',
+  },
+  referenceUrls_description: {
+    id: 'gdprcookiesettings-choice_referenceUrls_description',
+    defaultMessage: 'Type URL domains like "google.com", "youtube.com", ...',
   },
 });
 
@@ -47,13 +56,15 @@ const SingleChoiceWidget = ({ value, onChange, type }) => {
       />
 
       {type === 'profiling' && (
-        <ArrayWidget
+        <TokenWidget
           id="referenceUrls"
           title={intl.formatMessage(messages.referenceUrls)}
           value={value.referenceUrls}
           onChange={(n, v) => {
             onChange({ ...value, [n]: v });
           }}
+          placeholder={intl.formatMessage(messages.referenceUrls_placeholder)}
+          description={intl.formatMessage(messages.referenceUrls_description)}
         />
       )}
       <SingleChoiceTextMultilingualWidget
