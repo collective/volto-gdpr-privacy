6a273c1fa1e158584c7bfa971e9de9e2e821f5fe
Giulia Ghisini <giulia.ghisini@redturtle.it>
2024-12-24 12:38:27 +0100

    fix: imports

--- src/components/CookieBanner/CookieBanner.jsx
+++ src/components/CookieBanner/CookieBanner.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { defineMessages, useIntl } from 'react-intl';
-import { Icon } from '@plone/volto/components/theme/Icon/Icon';
+import Icon from '@plone/volto/components/theme/Icon/Icon';
 import clearSVG from '@plone/volto/icons/clear.svg';
 import { TextBlockView } from '@plone/volto-slate/blocks/Text';
 import { checkRichTextHasContent } from '../../helpers/richText';
